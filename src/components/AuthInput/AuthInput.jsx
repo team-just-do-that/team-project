0e6f6b05fe34a300dbb3a93227267ecfbaecddda
kwakjaehun <jh5414092@icloud.com>
2024-06-18 13:12:54 +0900

    Style : Input Component의 styled-component 분리

--- src/components/AuthInput/AuthInput.jsx
+++ src/components/AuthInput/AuthInput.jsx
@@ -1,32 +1,4 @@
-import styled from 'styled-components';
-
-const StDiv = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: flex-start;
-`;
-
-const StLabel = styled.label`
-  left: -120px;
-  width: 100px;
-  height: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-
-  margin-bottom: 0.5rem;
-`;
-
-const StInput = styled.input`
-  width: 300px;
-  height: 40px;
-  padding-left: 10px;
-  border: 1px solid #c0c0c0;
-  border-radius: 10px;
-  box-sizing: border-box;
-  background-color: #fcfdff;
-`;
+import { StDiv, StInput, StLabel } from './AuthInput.styled';
 
 export const AuthInput = ({ label, name, type, placeholder, value, onChange }) => {
   return (
