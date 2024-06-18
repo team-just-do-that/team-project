5afd860f80ddeb9767d5df75f295a005d21b1aae
kwakjaehun <jh5414092@icloud.com>
2024-06-18 13:07:19 +0900

    Style : 회원가입 페이지 styled-component 분리

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,51 +1,7 @@
 import { Page } from '@/Layouts/Page';
 import { AuthInput } from '@/components/AuthInput/AuthInput';
 import { useInputs } from '@/hooks/useInputs';
-import styled from 'styled-components';
-
-const StDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 20px;
-  width: 680px;
-  height: 600px;
-  border-radius: 20px;
-  background-color: #fcfdff;
-  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
-`;
-
-const StTitle = styled.h1`
-  font-size: 40px;
-  font-weight: 700;
-  margin-bottom: 40px;
-`;
-
-const StForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 20px;
-`;
-
-const StButton = styled.button`
-  width: 300px;
-  height: 40px;
-  background-color: #2d2d2d;
-  color: white;
-  border: none;
-  border-radius: 10px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-  opacity: 1;
-  margin-top: 20px;
-
-  &:hover {
-    opacity: 0.8;
-  }
-`;
+import { StButton, StDiv, StForm, StTitle } from './SignUp.styled';
 
 export const SignUp = () => {
   const [inputs, setInputs] = useInputs({
@@ -92,7 +48,7 @@ export const SignUp = () => {
             value={inputs.passwordCheck}
             onChange={setInputs}
           />
-          <StButton>가입하기</StButton>
+          <StButton type="submit">가입하기</StButton>
         </StForm>
       </StDiv>
     </Page>
