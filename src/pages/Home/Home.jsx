7c9745021055e52f2860f98b0d2f5fedba6b85b2
kwakjaehun <jh5414092@icloud.com>
2024-06-18 17:50:25 +0900

    Chore : 라우터 정리

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,24 @@
+import { signOutWithSupabase } from '@/api/api.auth';
+import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 import { StDiv } from './Home.styled';
 
 export const Home = () => {
-  return <StDiv>Home</StDiv>;
+  const navigate = useNavigate();
+  const { mutate: signOut } = useMutation({
+    mutationFn: signOutWithSupabase,
+    onError: () => {
+      console.log('error');
+    },
+    onSuccess: () => {
+      alert('로그아웃');
+      navigate('/');
+    }
+  });
+  return (
+    <>
+      <button onClick={signOut}>로그아웃</button>
+      <StDiv>Home</StDiv>
+    </>
+  );
 };
