c61577a02d31d4641f34c7277188e6a5e69315d0
minseo <mensung6@gmail.com>
2024-06-18 14:05:11 +0900

    Layout NavBar n Home create

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,13 +1,84 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom/dist';
+import styled from 'styled-components';
 
 function Layout() {
   return (
-    <div>
-      Layout
+    <StBackground>
+      <StHeader>
+        <StLogo src={imgsrc} />
+        <StNoneBodyBtn>둘러보기</StNoneBodyBtn>
+        <StNoneBodyBtn>모임만들기</StNoneBodyBtn>
+        <StNoneBodyBtn>마이페이지</StNoneBodyBtn>
+        <StSignDiv>
+          <>
+            <StSignBtn>로그아웃</StSignBtn>
+          </>
+          <>
+            <StSignBtn>로그인</StSignBtn>
+          </>
+        </StSignDiv>
+      </StHeader>
       <Outlet />
-    </div>
+    </StBackground>
   );
 }
+
+const StBackground = styled.section`
+  background-color: #fcfdff;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StHeader = styled.section`
+  height: 5rem;
+  background-color: #2d2d2d;
+  color: #fcfdff;
+  display: flex;
+  align-items: center;
+  padding: 5px 20px 5px 20px;
+`;
+
+// 높이 100px 하려고 했는데 못생겨서 75px 정도로 변경
+
+const StLogo = styled.img`
+  width: 340px;
+  height: 60px;
+  margin-right: 10px;
+`;
+
+// 반응형 생각했을 땐 로고 너비 20vw 하고 높이를 너비에 따라 고정되게 하면 좋을텐데 방법 찾아본 후 수정
+
+const imgsrc =
+  'https://media.discordapp.net/attachments/868151962623967276/1252433645688459294/boradmate.png?ex=6672333a&is=6670e1ba&hm=3f9290842f3d0008c8835a875662770a36d302c9b412d920b479a4df638c6c30&=&format=webp&quality=lossless';
+
+// 나중에 링크 말고 파일 위치로 수정할 예정
+
+const StNoneBodyBtn = styled.button`
+  margin: 0.2rem;
+  width: 5rem;
+  height: 2.2rem;
+  border-radius: 0.5rem;
+  color: #fcfdff;
+  background-color: transparent;
+  border: none;
+  font-weight: 700;
+`;
+
+const StSignDiv = styled.div`
+  margin-left: auto;
+`;
+
+const StSignBtn = styled.button`
+  width: 6.5rem;
+  height: 2.2rem;
+  margin-right: 0.2rem;
+  border-radius: 0.5rem;
+  background-color: #fcfdff;
+  color: #2d2d2d;
+  font-weight: 700;
+`;
 
 export default Layout;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,111 @@
 import { StDiv } from './Home.styled';
+import styled from 'styled-components';
 
 export const Home = () => {
-  return <StDiv>Home</StDiv>;
+  return (
+    <StDiv>
+      <StHomeSection>
+        <StSlideSection>슬라이드 아이템 들어가는 곳</StSlideSection>
+        <StCardsSection>
+          <StCardsCotainer>
+            <StCardsAlignBtn>▼ 최신순</StCardsAlignBtn>
+            <StCards>
+              <StCard>
+                <StCardImg />
+                <StTitle>제목</StTitle>
+                <StPlace>장소</StPlace>
+                <StPostItem>모집중</StPostItem>
+              </StCard>
+              <StCard>카드~</StCard>
+              <StCard>카드~</StCard>
+              <StCard>카드~</StCard>
+            </StCards>
+          </StCardsCotainer>
+        </StCardsSection>
+      </StHomeSection>
+    </StDiv>
+  );
 };
+
+const StHomeSection = styled.section`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  overflow-x: hidden;
+  overflow-y: auto;
+`;
+
+const StSlideSection = styled.section`
+  height: 500px;
+  background-color: pink;
+  display: flex;
+  justify-content: center;
+`;
+
+const StCardsSection = styled.section`
+  min-height: 300px;
+  display: flex;
+  justify-content: center;
+`;
+
+const StCardsCotainer = styled.div`
+  width: 80vw;
+`;
+
+const StCardsAlignBtn = styled.button`
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  width: 10rem;
+  height: 2.2rem;
+  border-radius: 0.5rem;
+  background-color: #fcfdff;
+  color: #2d2d2d;
+  font-weight: 700;
+`;
+
+const StCards = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(33.3%, auto));
+  grid-auto-rows: minmax(25rem, 0);
+  row-gap: 2rem;
+`;
+
+const StCard = styled.div`
+  background-color: gray;
+  width: 30rem;
+  height: 25rem;
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto 0 auto;
+  gap: 0.5rem;
+`;
+
+const StCardImg = styled.div`
+  background-color: black;
+  width: 28rem;
+  height: 16rem;
+  margin: 1rem auto 0.5rem auto;
+  border-radius: 0.5rem;
+`;
+
+const StTitle = styled.p`
+  margin-left: 1rem;
+  font-size: 2rem;
+`;
+
+const StPlace = styled.p`
+  margin-left: 1rem;
+  font-size: 1.5rem;
+`;
+
+const StPostItem = styled.div`
+  background-color: #2d2d2d;
+  color: #fcfdff;
+  width: 5.5rem;
+  height: 1.7rem;
+  border-radius: 0.5rem;
+`;
+
+// 스크롤 계속 생김 왜?
