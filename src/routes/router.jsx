439d22daaca509af4f93662baaf7cf502df3c9a6
Sol Park <solpark16@gmail.com>
2024-06-18 15:35:53 +0900

    Feat: crud 기능 제작

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,4 +1,5 @@
 import Layout from '@/Layouts/Layout';
+import CrudTest from '@/pages/CrudTest/CrudTest';
 import { Home } from '@/pages/Home';
 import { Children } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
@@ -8,5 +9,9 @@ export const router = createBrowserRouter([
     path: '/',
     element: <Layout />,
     children: [{ path: '', element: <Home /> }]
+  },
+  {
+    path: '/crudtest',
+    element: <CrudTest />
   }
 ]);
--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
       { find: '@query', replacement: '/src/query' },
       { find: '@redux', replacement: '/src/redux' },
       { find: '@routes', replacement: '/src/routes' },
-      { find: '@supabase', replacement: '/src/supabase' },
+      // { find: '@supabase', replacement: '/src/supabase' },
       { find: '@utils', replacement: '/src/utils' }
     ]
   }
